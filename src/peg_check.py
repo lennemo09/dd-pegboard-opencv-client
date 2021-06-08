62be28f0c5b965b58a052c95623148bdbca8fc6c
Nhan Luong <lennemo09@gmail.com>
2021-06-08 20:15:51 +1000

    Fixed wrong selection coordinates in pegcheck function.

--- src/peg_check.py
+++ src/peg_check.py
@@ -6,9 +6,9 @@ def checkIntensity(greyscale_frame,coords,intensity_threshold):
     greyscale_frame = cv2.resize(greyscale_frame,(VIDEO_W, VIDEO_H))
     _, mask2 = cv2.threshold(greyscale_frame, thresh=180, maxval=255, type=cv2.THRESH_BINARY)
     img_thresh_gray = cv2.bitwise_and(greyscale_frame, mask2)
-    selection = img_thresh_gray[coords[1]:coords[0], coords[3]:coords[2]]
+    selection = img_thresh_gray[coords[1]:coords[3], coords[0]:coords[2]]
     intensity = np.mean(selection)
-    print(f"{selection.shape},{coords[1]}:{coords[0]}, {coords[3]}:{coords[2]}",intensity)
+    #print(f"{greyscale_frame.shape},{selection.shape},{coords[1]}:{coords[3]}, {coords[0]}:{coords[2]}",intensity)
     if intensity>intensity_threshold:
         return True
     else:
